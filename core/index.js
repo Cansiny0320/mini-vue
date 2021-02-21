b0ba1e0e6734345151f7a7ed274d1ba8dc09dc85
Cansiny0320 <1136843910@qq.com>
2021-02-21 22:14:11 +0800

    feat: diff

--- core/index.js
+++ core/index.js
@@ -1,14 +1,26 @@
 import { effectWatch } from './reactivity/index.js'
-import { mountElement } from './renderer/index.js'
+import { mountElement, diff } from './renderer/index.js'
 export function creatApp(rootComponent) {
   return {
     mount(rootContainerSelector) {
       const context = rootComponent.setup()
       const rootContainer = document.querySelector(rootContainerSelector)
+      let isMounted = false
+      let preSubTree
       effectWatch(() => {
-        rootContainer.innerHTML = ``
-        const subTree = rootComponent.render(context)
-        mountElement(subTree, rootContainer)
+        if (!isMounted) {
+          // init
+          isMounted = true
+          const subTree = rootComponent.render(context)
+          mountElement(subTree, rootContainer)
+          preSubTree = subTree
+        } else {
+          // update
+          rootContainer.innerHTML = ``
+          const subTree = rootComponent.render(context)
+          diff(preSubTree, subTree)
+          preSubTree = subTree
+        }
       })
     },
   }
--- core/renderer/index.js
+++ core/renderer/index.js
@@ -1,7 +1,81 @@
+// n1 old vnode
+// n2 new vnode
+
+export function diff(n1, n2) {
+  // 1.tag
+  if (n1.tag !== n2.tag) {
+    n1.el.replaceWith(document.createElement(n2.tag))
+  } else {
+    const el = (n2.el = n1.el)
+    // 2.props
+    const { props: newProps } = n2
+    const { props: oldProps } = n1
+    if (newProps && oldProps) {
+      Object.keys(newProps).forEach(key => {
+        const newVal = newProps[key]
+        const oldVal = oldProps[key]
+        if (newVal !== oldVal) {
+          el.setAttribute(key, newVal)
+        }
+      })
+    }
+
+    if (oldProps) {
+      Object.keys(oldProps).forEach(key => {
+        if (!newProps[key]) {
+          el.removeAttribute(key)
+        }
+      })
+    }
+
+    // 3.children
+    const { children: newChildren } = n2
+    const { children: oldChildren } = n1
+
+    if (typeof newChildren === 'string') {
+      if (typeof oldChildren === 'string') {
+        if (oldChildren !== newChildren) {
+          el.textContent = newChildren
+        }
+      } else if (Array.isArray(oldChildren)) {
+        el.textContent = newChildren
+      }
+    } else if (Array.isArray(newChildren)) {
+      if (typeof oldChildren === 'string') {
+        el.textContent = ''
+        mountElement(n2, el)
+      } else if (Array.isArray(oldChildren)) {
+        const length = Math.min(newChildren.length, oldChildren.length)
+        for (let index = 0; index < length; index++) {
+          const oldVnode = oldChildren[index]
+          const newVnode = newChildren[index]
+          diff(oldVnode, newVnode)
+        }
+
+        if (newChildren.length > length) {
+          // 创建节点
+          for (let index = length; index < newChildren.length; index++) {
+            const newVnode = newChildren[index]
+            mountElement(newVnode, newVnode.el.parent)
+          }
+        }
+
+        if (oldChildren.length > length) {
+          // 删除节点
+          for (let index = length; index < oldChildren.length; index++) {
+            const oldVnode = oldChildren[index]
+            oldVnode.el.parent.removeChild(oldVnode.el)
+          }
+        }
+      }
+    }
+  }
+}
+
 export function mountElement(vnode, container) {
   const { tag, props, children } = vnode
 
-  const el = document.createElement(tag)
+  const el = (vnode.el = document.createElement(tag))
 
   for (const key in props) {
     const val = props[key]
